92b728702241f6f244f0a9ab0610393a034d9bf6
Samuel Kobe <samuelkobe@gmail.com>
2015-02-23 17:06:45 -0800

    changes to index, css and added js menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-$(document).delegate('nav.navigation ul li a', 'click', function(){
-	$('.active').removeClass('active');
-    $(this).addClass('active');
+//opens our mobile nav menu
+$(document).delegate('#button-mobile', 'click', function(){
+	$('#menu-mobile').toggleClass('open');
+	$('#button-mobile').toggleClass('open');
+	$('.container').toggleClass('open');
+});
+
+//make mobile nav menu open and closing swipe-able.
+var pad = document.getElementById("swipe-pad");
+$(function() {
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ 		Hammer(pad).on("swipeleft", function() {
+	    	$('#menu-mobile').removeClass("open");
+	    });
+	    
+	    Hammer(pad).on("swiperight", function() {
+	    	$('#menu-mobile').addClass("open");
+	    });
+	};
 });
